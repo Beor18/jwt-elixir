d7eb3ad7f3e2b34733b5e5ae258ed361ecbd7112
Fernando Lopez <fer.ragnar19@gmail.com>
2018-12-12 19:01:59 -0300

    Cifrado de contraseña y validación de campos

--- lib/jwtElixir/accounts/user.ex
+++ lib/jwtElixir/accounts/user.ex
@@ -1,20 +1,41 @@
 defmodule JwtElixir.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
+  alias JwtElixir.Accounts.User
 
+  import Comeonin.Bcrypt, only: [hashpwsalt: 1]
 
+# se agrega la lógica para las contraseñas de hash y la validación de datos de campo.
+  
   schema "users" do
     field :email, :string
     field :password_hash, :string
+    # Virtual fields
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
 
     timestamps()
   end
-
+  
   @doc false
-  def changeset(user, attrs) do
+  def changeset(%User{} = user, attrs) do
     user
-    |> cast(attrs, [:email, :password_hash])
-    |> validate_required([:email, :password_hash])
+    |> cast(attrs, [:email, :password_hash, :password_confirmation])
+    |> validate_required([:email, :password_hash, :password_confirmation])
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 8)
+    |> validate_confirmation(:password)
     |> unique_constraint(:email)
+    |> put_password_hash
+  end
+
+  defp put_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}}
+        ->
+          put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ ->
+          changeset
+    end
   end
 end
