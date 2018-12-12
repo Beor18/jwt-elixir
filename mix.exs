77e28ac9252fdc96442ecb30ace5bd3f83d8594b
Beor18 <fer.ragnar19@gmail.com>
2018-12-12 09:38:48 -0300

    agregado dependencias

--- mix.exs
+++ mix.exs
@@ -40,7 +40,10 @@ defmodule JwtElixir.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:comeonin, "~> 4.0"},
+      {:bcrypt_elixir, "~> 1.0"},
+      {:guardian, "~> 1.0"}
     ]
   end
 
