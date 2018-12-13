a9ff8015cbdb37e13ab89beb07e4626c6b944cd0
Fernando Lopez <fer.ragnar19@gmail.com>
2018-12-13 09:55:22 -0300

    token registro

--- lib/jwtElixir/accounts/accounts.ex
+++ lib/jwtElixir/accounts/accounts.ex
@@ -4,8 +4,9 @@ defmodule JwtElixir.Accounts do
   """
 
   import Ecto.Query, warn: false
+  import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
   alias JwtElixir.Repo
-
+  alias JwtElixir.Guardian
   alias JwtElixir.Accounts.User
 
   @doc """
@@ -17,6 +18,38 @@ defmodule JwtElixir.Accounts do
       [%User{}, ...]
 
   """
+  def token_sign_in(email, password) do
+    case email_password_auth(email, password) do
+      {:ok, user} ->
+        Guardian.encode_and_sign(user)
+      _ ->
+        {:error, :unauthorized}
+    end
+  end
+
+  defp email_password_auth(email, password) when is_binary(email) and is_binary(password) do
+    with {:ok, user} <- get_by_email(email),
+    do: verify_password(password, user)
+  end
+
+  defp get_by_email(email) when is_binary(email) do
+    case Repo.get_by(User, email: email) do
+      nil ->
+          dummy_checkpw()
+          {:error, "Login Error"}
+      user ->
+          {:ok, user}
+  end
+
+  # verifica input password
+  defp verify_password(password, %User{} = user) when is_binary(password) do
+    if checkpw(password, user.password_hash) do
+      {:ok, user}
+    else
+      {:error, :invalid_password}
+    end
+  end
+
   def list_users do
     Repo.all(User)
   end
