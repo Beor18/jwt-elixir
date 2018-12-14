bb5742b84e961d07c2a57f0c73cee9ec3e0d7c76
Fernando Lopez <fer.ragnar19@gmail.com>
2018-12-14 09:08:01 -0300

    Rutas protegidas

--- lib/jwtElixir_web/controllers/user_controller.ex
+++ lib/jwtElixir_web/controllers/user_controller.ex
@@ -20,11 +20,11 @@ defmodule JwtElixirWeb.UserController do
     end
   end
 
-  def show(conn, %{"id" => id}) do
-    user = Accounts.get_user!(id)
-    render(conn, "show.json", user: user)
+  def show(conn, _params) do
+     user = Guardian.Plug.current_resource(conn)
+     conn |> render("user.json", user: user)
   end
-
+  
   def update(conn, %{"id" => id, "user" => user_params}) do
     user = Accounts.get_user!(id)
 
--- lib/jwtElixir_web/router.ex
+++ lib/jwtElixir_web/router.ex
@@ -1,8 +1,14 @@
 defmodule JwtElixirWeb.Router do
   use JwtElixirWeb, :router
 
+  alias JwtElixir.Guardian
+
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  pipeline :jwt_authenticated do
+    plug Guardian.AuthPipeline
   end
 
   scope "/api/v1", JwtElixirWeb do
@@ -11,4 +17,11 @@ defmodule JwtElixirWeb.Router do
     post "/sign_up", UserController, :create
     post "/sign_in", UserController, :sign_in
   end
+
+  scope "/api/v1", JwtElixirWeb do
+    pipe_through [:api, :jwt_authenticated]
+
+    get "/my_user", UserController, :show
+  end
+  
 end
