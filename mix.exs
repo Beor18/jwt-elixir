a7566cf049780e33590405bbc6033c9af473edc4
Fernando Lopez <fer.ragnar19@gmail.com>
2019-01-02 19:44:09 -0300

    fix cors

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule JwtElixir.MixProject do
       {:comeonin, "~> 4.0"},
       {:bcrypt_elixir, "~> 1.0"},
       {:guardian, "~> 1.0"},
-      {:corsica, "~> 1.1.2"}
+      {:cors_plug, "~> 2.0"}
     ]
   end
 
