0d708ef3b851d98b0295a88019e7ceb0789afaaa
Fernando Lopez <fer.ragnar19@gmail.com>
2019-03-12 21:41:30 -0300

    cambios

--- lib/jwtElixir_web/views/changeset_view.ex
+++ lib/jwtElixir_web/views/changeset_view.ex
@@ -11,9 +11,9 @@ defmodule JwtElixirWeb.ChangesetView do
     Ecto.Changeset.traverse_errors(changeset, &translate_error/1)
   end
 
-  def render("error.json", %{changeset: changeset}) do
+  def render("error.json", %{}) do
     # When encoded, the changeset returns its errors
     # as a JSON object. So we just pass it forward.
-    %{errors: translate_errors(changeset)}
+    %{errors: %{name: "Nombre requerido!", price: "Precio Requerido"}}
   end
 end
