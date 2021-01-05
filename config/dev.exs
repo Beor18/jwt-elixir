a57563ac0f2f98cb5c8706d4372baf43dd5c0f8e
Fernando Lopez <lopezfer@globalhitss.com>
2021-01-05 15:37:55 -0300

    add docker postgresql

--- config/dev.exs
+++ config/dev.exs
@@ -49,8 +49,8 @@ config :phoenix, :plug_init_mode, :runtime
 
 # Configure your database
 config :jwtElixir, JwtElixir.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "jwtelixir_dev",
+  username: "stackly",
+  password: "elixirdb",
+  database: "stackly",
   hostname: "localhost",
   pool_size: 10
